644a15368a9f3a1c301560b5c9706e790c83c678
Pratiksha Navnath Nangare <pratikshanangare04@gmail.com>
2024-12-10 08:24:37 +0000

    set homepage for deploy github pages

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -10,7 +10,7 @@ import React from "react";
  * About background image
  *
  * Below is a sample image. Upload the image of your choice into the "images"
- * directory and import here for use. Then, set imageAltText to string that 
+ * directory and import here for use. Then set imageAltText to string that
  * represents what you see in that image.
  *
  * Need an image? Check out https://unsplash.com to download a image you
@@ -24,8 +24,7 @@ const imageAltText = "pink abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-"I'm a software developer passionate about creating seamless and innovative web applications. I enjoy working with both frontend and backend technologies to deliver efficient and user-friendly solutions.";
-
+  "I am a software developer passionate about building seamless and innovative web applications using both frontend and backend technologies.";
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
